d25da7d0a50c1c550a8d5fe2ad419464ac76bde9
JorgeVargasConnectUp <jorge.vargas@connectupweb.com>
2023-07-01 00:32:49 -0500

    [UPDATE] backend url

--- src/services/axios.instances.js
+++ src/services/axios.instances.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const API_BASE_URL = import.meta.env.VITE_API_EXPRESS_URL
-const API_BASE_URL = 'http://localhost:5000'
+const API_BASE_URL = `http://${window.location.hostname}:5000`
 
 const configBasic = {
   baseURL: API_BASE_URL,
