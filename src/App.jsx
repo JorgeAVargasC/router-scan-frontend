aadc0a2dbe4ee48850fe9bb0b7f2b9a036589ce7
JorgeAVargasC <javargas1209@gmail.com>
2023-08-28 23:26:01 -0500

    [UPDATE] i18n

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { AppRoutes } from './routes'
+import Backend from 'i18next-http-backend'
 
 import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
@@ -10,11 +11,15 @@ import { i18nState } from '@/contexts/i18n.context'
 import { useRecoilState } from 'recoil'
 export default function App() {
 
-
 const [i18n] = useRecoilState(i18nState)
 
-i18next.use(initReactI18next).init({
+i18next
+.use(Backend)
+.use(initReactI18next)
+.init({
   lng: i18n,
+  fallbackLng: i18n,
+  debug: true,
   resources: {
     en: {
       translation: en,
