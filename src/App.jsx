9db3bcfe68e9608e39573aa550807c9fcbfde83e
JorgeAVargasC <javargas1209@gmail.com>
2023-08-30 22:32:50 -0500

    [UPDATE] google translate button

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { AppRoutes } from './routes'
-import Backend from 'i18next-http-backend'
+import { i18nState } from '@/contexts/i18n.context'
+
+import { initReactI18next } from 'react-i18next'
 
 import i18next from 'i18next'
-import { initReactI18next } from 'react-i18next'
+import Backend from 'i18next-http-backend'
+import { useRecoilState } from 'recoil'
+
+import { AppRoutes } from './routes'
 
 import en from './i18n/en.json'
 import es from './i18n/es.json'
 
-import { i18nState } from '@/contexts/i18n.context'
-import { useRecoilState } from 'recoil'
 export default function App() {
+  const [i18n] = useRecoilState(i18nState)
 
-const [i18n] = useRecoilState(i18nState)
-
-i18next
-.use(Backend)
-.use(initReactI18next)
-.init({
-  lng: i18n,
-  fallbackLng: i18n,
-  debug: true,
-  resources: {
-    en: {
-      translation: en,
-    },
-    es:{
-      translation: es,
-    }
-  },
-})
+  i18next
+    .use(Backend)
+    .use(initReactI18next)
+    .init({
+      lng: i18n,
+      fallbackLng: i18n,
+      debug: true,
+      resources: {
+        en: {
+          translation: en,
+        },
+        es: {
+          translation: es,
+        },
+      },
+    })
 
   return <AppRoutes />
 }
